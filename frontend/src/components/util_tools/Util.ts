bb2fb7a510e55c6c422b2f307a9e33f356a5fca1
Paul Bekaert <2517073B@student.gla.ac.uk>
2025-02-23 15:14:28 +0000

    fixed frontend issue

--- frontend/src/components/util_tools/Util.ts
+++ frontend/src/components/util_tools/Util.ts
@@ -1,8 +1,8 @@
 export const formatArticleContent = (content: string) => {
-  return content
+  return content?.length ? content
     .replace(/\\u([a-fA-F0-9]{4})/g, (match, group) => 
       String.fromCharCode(parseInt(group, 16)) 
     )
     .replace(/(?:\\r\\n|\\r|\\n)/g, '<br>')
-    .replace(/\\"/g, '"');
+    .replace(/\\"/g, '"') : "";
 }
